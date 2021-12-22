79c08882680da2056905c150abcbaf8fc6612740
dugksl23 <dugksl23@gmail.com>
2021-12-22 15:47:10 +0900

    feat : 값 타입 컬렉션과 @ElementCollection과 @CollectionTable

--- src/main/java/com/example/jpabasic/practice_exam/MemberExam.java
+++ src/main/java/com/example/jpabasic/practice_exam/MemberExam.java
@@ -9,7 +9,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -42,4 +45,20 @@ public class MemberExam extends BaseEntity {
             @AttributeOverride(name = "city", column = @Column(name = "work_city")),
             @AttributeOverride(name = "street", column = @Column(name = "work_street"))})
     private Address workAddress;
+
+
+    @ElementCollection
+    @CollectionTable(name = "food", joinColumns = @JoinColumn(name = "memberExam_id"))
+    // @OneToMany와 같은 1:다의 관계, food 테이블의 관계의 주인
+    // 반면 food table @ManyToOne(mappedBy="member_id")으로 관계의 주인이지만,
+    // @JoinColumn(name = "member_id", , insertable = false, updatable = false)
+    // 이건 마치 1:다의 @OneToMany 에서 관계의 주인은 One이지만, db에서는 다 쪽에 PK가 가기에
+    // 다쪽이 되는 테이블에서 @JoinColumn(insertable = false, updatable = false) 와 비슷
+    // 1:다의 관계에서 다에서는 관계의 주인을 나타내는 mappedBy는 one에서 정의하기에 one이 주인일 때에는
+    // mappedBy 속성 사용할 수 없는 것과 현재 이 상황이 유사
+    private Set<String> favoriteFood = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "addresses", joinColumns = @JoinColumn(name = "member_id"))
+    private List<Address> addresses = new ArrayList<>();
 }
